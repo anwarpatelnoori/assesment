8ed943a5805e0fa6ccacb0686cdd8a51664d8ecf
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2025-08-31 19:13:29 +0000

    Restore changes from stash after repo corruption

--- assesment/hooks.py
+++ assesment/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/assesment/css/assesment.css"
-app_include_js = "assesment.agency_management.doctype.agency.cutsom_nav_bar1.js"
+# app_include_js = "/assets/assesment/css/assesment.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/assesment/css/assesment.css"
